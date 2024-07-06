2449fc91ca82d93c14809aae5fdc9de392c82332
niraj <niraj@mapmyindia.com>
2024-07-06 15:11:43 +0530

    theme switch button ring removed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -27,7 +27,7 @@ export default function NavBar({}) {
           <label className="inline-flex items-center cursor-pointer" title="dark/light">
             <input type="checkbox" value="" className="sr-only peer" onChange={toggleTheme} />
             <div
-              className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-bg-accentEmphasis peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border after:rounded-full after:h-4 after:w-4 after:transition-all  peer-checked:bg-black"
+              className="relative w-9 h-5  peer-focus:outline-none rounded-full peer dark:bg-bg-accentEmphasis peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border after:rounded-full after:h-4 after:w-4 after:transition-all  peer-checked:bg-black"
             ></div>
           </label>
         </div>
